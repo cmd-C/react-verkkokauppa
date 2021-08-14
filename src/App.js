bc781c6aae4db72b1bb007d947d91101c90fc834
sahar <sah.rass@outlook.com>
2021-08-14 20:07:48 +0300

    tehnyt pohjan

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
+
+function Tuotesivu(props) {
+  return (
+    <>
+    <img src={props.kuva} alt="tuotekuva" width="500" height="500"/>
+    <h1>{props.hinta}</h1>
+    </>
+  )
+}
+
+function Tuotenappi(props) {
+  return (
+    <div>
+    <button type="button"><h2>{props.nimi}</h2><img src={props.kuva} width="200" lenght="200"></img></button>
+    </div>
+  )
+}
+
+function Lista(props) {
+  return (
+    <div>
+      <h1>VERKKOKAUPPA</h1>
+      <h2>kauppa, josta löytyy kaikki, mitä päivöliinit tarvitsevat tavallisiin elintoimintoihin!</h2>
+      {props.tuotteet.map(function(tuote){
+        return(
+      <Link to={"/"+tuote.nimi}>
+        <Tuotenappi nimi={tuote.nimi} kuva={tuote.kuva}></Tuotenappi>
+      </Link>
+
+        )
+      })}
+    </div>
+
+  )
+}
 
 function App() {
+
+  const tuotteet=[
+    {
+      nimi: "sola", 
+      kuva: "sola.jpg",
+      hinta: "2,99€" 
+    },
+    {
+      nimi: "nuudeli",
+      kuva: "nuudeli.jpg",
+      hinta: "0,99€"
+    },
+    {
+      nimi: "jumalallinen visio",
+      kuva: "jeesus.jpeg",
+      hinta: "Hinta: sielu"
+    },
+    {
+      nimi: "Wolfram Alpha",
+      kuva: "wolfram.jpg",
+      hinta: "7,99€/kk"
+    }
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Lista tuotteet={tuotteet}></Lista>
+          </Route>
+          {tuotteet.map(function(tuote){
+            return (<Route path={"/"+tuote.nimi}>
+                <Tuotesivu kuva={tuote.kuva} hinta={tuote.hinta}/>
+              </Route>)
+          }
+          )}
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+    return (
+        <>
+        <nav className="Navbar">
+          <div className="navbar-container">
+              <Link to="/" className=""></Link>
+              </div>  
+        </nav>
+        </>
+    )
+}
+
+export default Navbar
