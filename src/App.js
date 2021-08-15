f8011f8cc84c871eb3a617d8cf78780198d37476
sahar <sah.rass@outlook.com>
2021-08-15 11:24:54 +0300

    stonks

--- src/App.js
+++ src/App.js
@@ -1,74 +1,72 @@
-import React from 'react';
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-} from "react-router-dom";
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function Tuotesivu(props) {
   return (
     <>
-    <img src={props.kuva} alt="tuotekuva" width="500" height="500"/>
-    <h1>{props.hinta}</h1>
+      <img src={props.kuva} alt="tuotekuva" width="500" height="500" />
+      <h1>{props.hinta}</h1>
     </>
-  )
+  );
 }
 
 function Tuotenappi(props) {
   return (
     <div>
-    <button type="button"><h2>{props.nimi}</h2><img src={props.kuva} width="200" lenght="200"></img></button>
+      <button type="button">
+        <h2>{props.nimi}</h2>
+        <img src={props.kuva} width="200" lenght="200"></img>
+      </button>
     </div>
-  )
+  );
 }
 
 function Lista(props) {
   return (
     <div>
       <h1>VERKKOKAUPPA</h1>
-      <h2>kauppa, josta löytyy kaikki, mitä päivöliinit tarvitsevat tavallisiin elintoimintoihin!</h2>
-      {props.tuotteet.map(function(tuote){
-        return(
-      <Link to={"/"+tuote.nimi}>
-        <Tuotenappi nimi={tuote.nimi} kuva={tuote.kuva}></Tuotenappi>
-      </Link>
-
-        )
+      <h2>
+        kauppa, josta löytyy kaikki, mitä päivöliinit tarvitsevat tavallisiin
+        elintoimintoihin!
+      </h2>
+      {props.tuotteet.map(function (tuote) {
+        return (
+          <Link to={"/" + tuote.nimi}>
+            <Tuotenappi nimi={tuote.nimi} kuva={tuote.kuva}></Tuotenappi>
+          </Link>
+        );
       })}
       <Link to="/pepsi">
         <Tuotenappi nimi="pepsi" kuva="pepsi.jpg"></Tuotenappi>
       </Link>
     </div>
-
-  )
+  );
 }
 
 function App() {
-
-  const tuotteet=[
+  const tuotteet = [
     {
-      nimi: "sola", 
+      nimi: "sola",
       kuva: "sola.jpg",
-      hinta: "2,99€" 
+      hinta: "2,99€",
     },
     {
       nimi: "nuudeli",
       kuva: "nuudeli.jpg",
-      hinta: "0,99€"
+      hinta: "0,99€",
     },
     {
       nimi: "jumalallinen visio",
       kuva: "jeesus.jpeg",
-      hinta: "Hinta: sielu"
+      hinta: "Hinta: sielu",
     },
     {
       nimi: "Wolfram Alpha",
       kuva: "wolfram.jpg",
-      hinta: "7,99€/kk"
-    }
-  ]
+      hinta: "7,99€/kk",
+    },
+  ];
   return (
     <div className="App">
       <Router>
@@ -76,14 +74,15 @@ function App() {
           <Route exact path="/">
             <Lista tuotteet={tuotteet}></Lista>
           </Route>
-          {tuotteet.map(function(tuote){
-            return (<Route path={"/"+tuote.nimi}>
-                <Tuotesivu kuva={tuote.kuva} hinta={tuote.hinta}/>
-              </Route>)
-          }
-          )}
-           <Route path="/pepsi">
-            <img src="gameover.jpg" width="600"/>
+          {tuotteet.map(function (tuote) {
+            return (
+              <Route path={"/" + tuote.nimi}>
+                <Tuotesivu kuva={tuote.kuva} hinta={tuote.hinta} />
+              </Route>
+            );
+          })}
+          <Route path="/pepsi">
+            <div className="pepsi"></div>
           </Route>
         </Switch>
       </Router>
